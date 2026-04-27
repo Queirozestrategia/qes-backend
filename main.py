f848e2f91bcbde4466d22988e1deedebb7355277
Queirozestrategia <contato@queirozestrategia.com.br>
2026-04-27 01:40:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-import os
 
 app = FastAPI()
 
-# Caminho base absoluto
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# CORS (liberado para testes)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -17,32 +10,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# 🔥 SERVIR LOGO (corrigido)
-app.mount(
-    "/assets",
-    StaticFiles(directory=os.path.join(BASE_DIR, "../frontend/assets")),
-    name="assets"
-)
-
-# app.mount(
-#     "/assets",
-#     StaticFiles(directory=os.path.join(BASE_DIR, "../frontend/assets")),
-#     name="assets"
-# )
-
-# LOGIN
-@app.post("/login")
-async def login(user: dict):
-    if user.get("username") == "admin" and user.get("password") == "123":
-        return {"status": "ok"}
-    return {"status": "erro"}
-
-# FRONTEND
 @app.get("/")
 def home():
-    return FileResponse(os.path.join(BASE_DIR, "../frontend/index.html"))
+    return {"status": "API online"}
 
-# DADOS
 @app.get("/dashboard/")
 def dashboard():
     return {
